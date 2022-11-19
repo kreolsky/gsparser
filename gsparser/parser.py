17f36518da99662532c0279a2d9f74ce57537b36
Sergey Zaigraev <zaigraeff@gmail.com>
2022-11-20 00:23:45 +0300

    beautify

--- gsparser/parser.py
+++ gsparser/parser.py
@@ -20,8 +20,8 @@ def parse_command(command, result):
     """
     Парсер команд, которые будут применены к содержимому ключа.
     В данный момент поддерживает команды:
-    'list' -- заворачить содержимое в список если это не список
-    'flist' -- всегда заворачивает в список!
+    'list' - заворачить содержимое в список если это не список
+    'flist' - всегда заворачивает в список, даже списки!
     """
 
     if not command:
@@ -40,37 +40,32 @@ def parse_block(string, **params):
     Возвращает список с элементами конфига, обычно это словари.
     """
 
-    br_block = params['br_block']
-    sep_base = params['sep_base']
-    sep_dict = params['sep_dict']
-    to_num = params['to_num']
-    raw_pattern = params['raw_pattern']
     is_mode_v2 = params['mode'] == 'v2'
-
     out = []
     out_dict = {}
 
     # Строка делится на фрагменты по базовому разделителю элементов
-    for line in tools.split_string_by_sep(string, sep_base, **params):
+    for line in tools.split_string_by_sep(string, params['sep_base'], **params):
         # Нужно ли вообще парсить фрагмент или вернуть как есть
-        if line.startswith(raw_pattern):
+        if line.startswith(params['raw_pattern']):
             out.append(line[1:-1])
 
         # Проверка наличия блока. Всегда начинается с открывающей скобки блока
-        elif line.startswith(br_block[0]):
+        elif line.startswith(params['br_block'][0]):
             # Отрезаем скобки и парсим содержимое
             # Внутренний блок всегда разворачиваем из списков, иначе паразитные вложения
             substring = config_to_json(line[1:-1], _unwrap_it=True, **params)
             out.append(substring)
 
         # Проверка на словарь
-        elif sep_dict in line:
+        elif params['sep_dict'] in line:
             # Когда мы пришли из словаря нужно проверить надо ли его вытаскивать из списка
-            # v1. Всегда False. Никогда НЕ разворачиваем, команды не поддерживает
+            # v1. Всегда False. Никогда НЕ разворачиваем. Команды не поддерживает
             # v2. Всегда True. Разворачиваем и применяем действие переданной команды
             unwrap_it = True if is_mode_v2 else False
             command = None
-            key, substring = tools.split_string_by_sep(line, sep_dict, **params)
+
+            key, substring = tools.split_string_by_sep(line, params['sep_dict'], **params)
             if is_mode_v2 and params['sep_func'] in key:
                 key, command = key.split(params['sep_func'])
 
@@ -79,7 +74,7 @@ def parse_block(string, **params):
 
         # Остались только строки
         else:
-            out.append(tools.parse_string(line, to_num))
+            out.append(tools.parse_string(line, params['to_num']))
 
     if out_dict:
         out.append(out_dict)
@@ -200,12 +195,6 @@ def config_to_json(string: str, _unwrap_it=None, **params) -> dict:
     Строка {one, {two, 3, 4}, {one = the choose one!}} дасть идентичный верхнему результат.
     Может пригодиться для задания, например, пустых списков или простых конструкций.
 
-    ### sep_func
-    Разделитель, через который пишется дополнительные команды для ключей. '!' по умолчанию.
-
-    Например: key!list означает, что содержимое ключа key обязательно будет списком.
-    ВАЖНО! Заворачивает только НЕ СПИСКИ. Список не будет дополнительно завернут.
-
     ### sep_base
     Базовый разделитель элементов. ',' по умолчанию.
 
@@ -219,9 +208,15 @@ def config_to_json(string: str, _unwrap_it=None, **params) -> dict:
     будет идентична:
     '{0, 6}, {7 = 7, zr = 0, one, tw = {2 = d}, tv = {{2 = dv}, {3 = tr}}}, {a, b}',
 
+    ### sep_func
+    Разделитель указания функций. '!' по умолчанию.
+    Например: key!list означает, что содержимое ключа key обязательно будет списком.
+
+    См. все доступные команды в parse_command()
+
     ### to_num
     Нужно ли пытаться преобразовывать значения в числа.
-    True (по умолчанию) пытается преобразовать.
+    True (по умолчанию) - пытается преобразовать.
 
     ### raw_pattern
     Символ маркирующий строку которую не нужно разбирать,
@@ -234,8 +229,7 @@ def config_to_json(string: str, _unwrap_it=None, **params) -> dict:
     True - не парсит, возвращает как есть.
 
     ### _unwrap_it
-    Указатель, что пришли из внутреннего блока который
-    всегда нужно разворачивать.
+    Указание нужно ли разворачивать получившуюся после парсинга структуру.
     """
 
     # Выйти сразу если не нужно парсить
@@ -247,21 +241,19 @@ def config_to_json(string: str, _unwrap_it=None, **params) -> dict:
 
     # Только при первом запуске
     if _unwrap_it is None:
-        unwrap_init_dict = {
-            'v1': True,
-            'v2': True }
+        # Настройка разворачивания самого верхнего уровня
+        init_unwrap = {'v1': True, 'v2': True }
+        _unwrap_it = init_unwrap[params['mode']]
 
-        params['br_block'] = params.get('br_block', '{}')
         params['br_list'] = '[]'  # Нельзя переопределять формат скобок питонячих списков!
-        params['sep_func'] = params.get('sep_func', '!')  # Дополнительные команды для ключей
+        params['br_block'] = params.get('br_block', '{}')
+        params['sep_func'] = params.get('sep_func', '!')
         params['sep_block'] = params.get('sep_block', '|')
         params['sep_base'] = params.get('sep_base', ',')
         params['sep_dict'] = params.get('sep_dict', '=')
         params['raw_pattern'] = params.get('raw_pattern', '"')
         params['to_num'] = params.get('to_num', True)
         params['mode'] = params.get('mode', 'v1')
-
-        _unwrap_it = unwrap_init_dict[params['mode']]
 
     out = []
     for line in tools.split_string_by_sep(string, params['sep_block'], **params):
